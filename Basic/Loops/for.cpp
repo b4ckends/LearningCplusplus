91a4a2106a8fae196a6b8394933f593cccbd9057
rs3c <tartaacanon@gmail.com>
2023-08-14 19:35:39 -0600

    Loop for

---
+++ Basic/Loops/for.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int list[] = { 100, 200, 300 };
+	int limit = sizeof(list) / sizeof(list[0]);
+	for (int i=0; i<limit; i+=1) {
+		cout << list[i] << endl;
+		if (list[i] == 200) {
+			break;
+		}
+	}
+}
