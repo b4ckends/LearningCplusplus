be9f7cbae9653c04366d04749e51026a1cc17446
rs3c <tartaacanon@gmail.com>
2023-08-14 19:58:43 -0600

    Functions

---
+++ Basic/Functions/functions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int addition(int a, int b) {
+	return a + b;
+}
+
+int times(int a, int b) {
+	return a * b;
+}
+
+int main() {
+	cout << addition(94, 86) << endl;
+	cout << times(50, 50) << endl;
+
+}
