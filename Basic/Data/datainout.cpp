05d5e7187ca8fcfa672fa5b0fc23d4bf4b42f427
rs3c <tartaacanon@gmail.com>
2023-08-14 18:30:42 -0600

    Data in out

---
+++ Basic/Data/datainout.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// yolooooooooooo = german 2014
+	/*
+	  I miss 
+	*/
+	int age = 0;
+	cout << "Age: ";
+	cin >> age;
+	cout << "Your age is: " << age;
+}
